74d91480a7b975aa226a8b3e0ad6a3d5767798e6
Rashy-hub <yagoubi.r@gmail.com>
2024-08-08 19:31:46 +0200

    render external url env updated

--- utils/wakupself.js
+++ utils/wakupself.js
@@ -1,14 +1,14 @@
 const axios = require('axios')
 // Fonction pour faire une requête à soi-même
 //SELF_URL='http:/localhost:8080/api/wakeup'
-const { SELF_URL } = process.env
+
 const wakeUpSelf = async () => {
     try {
-        const response = await axios.get(SELF_URL)
+        const response = await axios.get(`https://${process.env.RENDER_EXTERNAL_URL}`)
 
-        console.log(`Requête envoyée à ${SELF_URL} pour éviter la mise en veille ${response.data}`)
+        console.log(`Requête envoyée à ${`https://${process.env.RENDER_EXTERNAL_URL}`} pour éviter la mise en veille ${response.data}`)
     } catch (error) {
-        console.error(`Erreur lors de la requête à ${SELF_URL}:`, error)
+        console.error(`Erreur lors de la requête à ${`https://${process.env.RENDER_EXTERNAL_URL}`}:`, error)
     }
 }
 
